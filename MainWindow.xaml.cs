0877732f2ad7537499a3926ffe63c32167021987
BeanZ <92383674+BeanZ141@users.noreply.github.com>
2024-05-18 22:07:30 +0530

    Updated timer function (working)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,48 +1,82 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace Stopwatch
 {
     public partial class MainWindow : Window
     {
-        int timeCs, timeS, timeMin;
-        bool isActive;
+        private int timeCs, timeSec, timeMin;
+        private bool isActive;
+        private DispatcherTimer timer;
 
         public MainWindow()
         {
             InitializeComponent();
+            InitializeTimer();
         }
 
-        private void MainWindow_Load(object sender, EventArgs e)
+        private void InitializeTimer()
         {
-            timeCs = 0;
-            timeS = 0;
-            timeMin = 0;
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Tick += Timer_Tick;
+        }
 
-            isActive = true;
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (isActive)
+            {
+                timeCs++;
+
+                if (timeCs >= 100)
+                {
+                    timeSec++;
+                    timeCs = 0;
+
+                    if (timeSec >= 60)
+                    {
+                        timeMin++;
+                        timeSec = 0;
+                    }
+                }
+            }
+
+            DrawTime();
+        }
+
+        private void DrawTime()
+        {
+            lblCs.Content = timeCs.ToString("00");
+            lblSec.Content = timeSec.ToString("00");
+            lblMin.Content = timeMin.ToString("00");
         }
 
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
             isActive = true;
+            timer.Start();
         }
 
         private void btnStop_Click(object sender, RoutedEventArgs e)
         {
             isActive = false;
+            timer.Stop();
         }
 
         private void btnReset_Click(object sender, RoutedEventArgs e)
         {
             isActive = false;
-
+            timer.Stop();
             ResetTime();
+            DrawTime();
         }
 
         private void ResetTime()
         {
             timeCs = 0;
-            timeS = 0;
+            timeSec = 0;
             timeMin = 0;
         }
 
